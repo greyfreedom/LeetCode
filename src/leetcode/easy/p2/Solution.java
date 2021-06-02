800b37a8bef951591330be3166e2eda0c0fc8cbe
wangpeng121 <wangpeng121@meituan.com>
2021-06-02 10:10:58 +0800

    ac p2

---
+++ src/leetcode/easy/p2/Solution.java
@@ -0,0 +1,32 @@
+package src.leetcode.easy.p2;
+
+import src.common.ListNode;
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
+            int sum = a + b + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+        }
+        if (carry != 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
