86541736c8cbedd70a3610a5a54bb67640cc9b8c
wangpeng121 <wangpeng121@meituan.com>
2021-06-04 19:40:34 +0800

    ac p88

--- src/leetcode/easy/p88/Solution.java
+++ src/leetcode/easy/p88/Solution.java
@@ -2,14 +2,24 @@ package src.leetcode.easy.p88;
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0) {
-            return;
-        }
-        if (m == 0) {
-            for (int i = 0; i < n; i++) {
-                nums1[i] = nums2[i];
+        int i1 = m - 1, i2 = n - 1, j = m + n - 1;
+        while (j >= 0) {
+            int cur;
+            if (i1 < 0) {
+                cur = nums2[i2];
+                i2--;
+            } else if (i2 < 0) {
+                cur = nums1[i1];
+                i1--;
+            } else if (nums1[i1] <= nums2[i2]) {
+                cur = nums2[i2];
+                i2--;
+            } else {
+                cur = nums1[i1];
+                i1--;
             }
+            nums1[j] = cur;
+            j--;
         }
-
     }
 }
