0f6b463ce3e8ba56016db06437002c673a1f24fc
wangpeng121 <wangpeng121@meituan.com>
2021-07-27 19:53:10 +0800

    ac p209

---
+++ src/leetcode/medium/p209/Solution.java
@@ -0,0 +1,17 @@
+package src.leetcode.medium.p209;
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0, res = Integer.MAX_VALUE;
+        int sum = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum >= target) {
+                res = Math.min(res, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return res < Integer.MAX_VALUE ? res : 0;
+    }
+}
