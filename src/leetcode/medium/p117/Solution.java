e74677ff7dc038f7958ed66f572081f399255f8a
turisla <turislandrose@gmail.com>
2021-08-12 16:37:47 +0800

    ac p117

---
+++ src/leetcode/medium/p117/Solution.java
@@ -0,0 +1,48 @@
+package src.leetcode.medium.p117;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+
+class Solution {
+    public Node connect(Node root) {
+        Deque<Node> queue = new ArrayDeque<>();
+        if (root != null) {
+            queue.addLast(root);
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node last = null;
+            for (int i = 0; i < size; i++) {
+                Node node = queue.removeFirst();
+                if (last != null) {
+                    last.next = node;
+                }
+                last = node;
+                if (node.left != null) queue.addLast(node.left);
+                if (node.right != null) queue.addLast(node.right);
+            }
+        }
+        return root;
+    }
+}
