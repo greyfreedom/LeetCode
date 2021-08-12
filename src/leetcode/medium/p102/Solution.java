5a1ddb8bf13d1433919025dec63edeb41e096816
turisla <turislandrose@gmail.com>
2021-08-12 15:29:26 +0800

    ac p102

---
+++ src/leetcode/medium/p102/Solution.java
@@ -0,0 +1,34 @@
+package src.leetcode.medium.p102;
+
+import src.common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        if (root != null) {
+            queue.addLast(root);
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> res = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.removeFirst();
+                res.add(node.val);
+                if (node.left != null) {
+                    queue.addLast(node.left);
+                }
+                if (node.right != null) {
+                    queue.addLast(node.right);
+                }
+            }
+            result.add(res);
+        }
+        return result;
+    }
+}
