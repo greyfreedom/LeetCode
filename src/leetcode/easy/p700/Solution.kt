e9ec3da023c511db31c3f3f8306c8d398e8b3af9
turisla <turislandrose@gmail.com>
2021-08-16 17:54:49 +0800

    使用迭代实现BST中查找值

--- src/leetcode/easy/p700/Solution.kt
+++ src/leetcode/easy/p700/Solution.kt
@@ -3,15 +3,33 @@ package src.leetcode.easy.p700
 import src.common.TreeNode
 
 class Solution {
-    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+    fun searchBST2(root: TreeNode?, `val`: Int): TreeNode? {
         return if (root == null || root.`val` == `val`) {
             root
         } else if (root.`val` > `val`) {
-            searchBST(root.left, `val`)
+            searchBST2(root.left, `val`)
         } else if (root.`val` < `val`) {
-            searchBST(root.right, `val`)
+            searchBST2(root.right, `val`)
         } else {
             null
         }
+    }
+
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        var node = root
+        while (node != null) {
+            node = when {
+                node.`val` == `val` -> {
+                    return node
+                }
+                node.`val` > `val` -> {
+                    node.left
+                }
+                else -> {
+                    node.right
+                }
+            }
+        }
+        return null
     }
 }
