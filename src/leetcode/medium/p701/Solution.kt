e82a0739964064b1b8bcef39c0360c9bbb1cabc8
turisla <turislandrose@gmail.com>
2021-08-17 13:20:30 +0800

    ac p701

--- src/leetcode/medium/p701/Solution.kt
+++ src/leetcode/medium/p701/Solution.kt
@@ -3,15 +3,34 @@ package src.leetcode.medium.p701
 import src.common.TreeNode
 
 class Solution {
-    fun insertIntoBST(root: TreeNode?, `val`: Int): TreeNode? {
+    fun insertIntoBST2(root: TreeNode?, `val`: Int): TreeNode? {
         if (root == null) {
             return TreeNode(`val`)
         }
         if (root.`val` > `val`) {
-            root.left = insertIntoBST(root.left, `val`)
+            root.left = insertIntoBST2(root.left, `val`)
         } else {
-            root.right = insertIntoBST(root.right, `val`)
+            root.right = insertIntoBST2(root.right, `val`)
         }
         return root
+    }
+
+    fun insertIntoBST(root: TreeNode?, `val`: Int): TreeNode? {
+        var cur = root
+        var pre: TreeNode? = null
+        while (cur != null) {
+            pre = cur
+            cur = if (cur.`val` > `val`) cur.left else cur.right
+        }
+        return if (pre != null) {
+            if (pre.`val` > `val`) {
+                pre.left = TreeNode(`val`)
+            } else {
+                pre.right = TreeNode(`val`)
+            }
+            root
+        } else {
+            TreeNode(`val`)
+        }
     }
 }
