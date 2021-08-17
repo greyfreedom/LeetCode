2dd146a3aae34314b9ee720e44778d26ba2c2d2a
turisla <turislandrose@gmail.com>
2021-08-17 14:17:12 +0800

    ac p699 迭代

--- src/leetcode/medium/p669/Solution.java
+++ src/leetcode/medium/p669/Solution.java
@@ -3,16 +3,42 @@ package src.leetcode.medium.p669;
 import src.common.TreeNode;
 
 class Solution {
-    public TreeNode trimBST(TreeNode root, int low, int high) {
+    public TreeNode trimBST2(TreeNode root, int low, int high) {
         if (root == null) return null;
         if (root.val < low) {
-            return trimBST(root.right, low, high);
+            return trimBST2(root.right, low, high);
         } else if (root.val > high) {
-            return trimBST(root.left, low, high);
+            return trimBST2(root.left, low, high);
         } else {
-            root.left = trimBST(root.left, low, high);
-            root.right = trimBST(root.right, low, high);
+            root.left = trimBST2(root.left, low, high);
+            root.right = trimBST2(root.right, low, high);
             return root;
         }
+    }
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return null;
+        while (root != null && (root.val < low || root.val > high)) {
+            if (root.val < low) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        TreeNode cur = root;
+        while (cur != null) {
+            while (cur.left != null && (cur.left.val < low)) {
+                cur.left = cur.left.right;
+            }
+            cur = cur.left;
+        }
+        cur = root;
+        while (cur != null) {
+            while (cur.right != null && (cur.right.val > high)) {
+                cur.right = cur.right.left;
+            }
+            cur = cur.right;
+        }
+        return root;
     }
 }
