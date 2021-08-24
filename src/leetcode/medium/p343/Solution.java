7ad6ff57a07762827fe22806bc9ea1efa56c6dc5
turisla <turislandrose@gmail.com>
2021-08-24 17:04:48 +0800

    ac p343

---
+++ src/leetcode/medium/p343/Solution.java
@@ -0,0 +1,15 @@
+package src.leetcode.medium.p343;
+
+class Solution {
+    public int integerBreak(int n) {
+        if (n <= 2) return 1;
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i - 1; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
